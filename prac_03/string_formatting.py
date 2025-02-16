1b90fac2d02041801ef3000392d915b6f1af9d39
João Gabriel R Costa <jgcosta.estudante@gmail.com>
2025-02-16 12:39:26 +0800

    "Implement loop for power of 2 calculations"

--- prac_03/string_formatting.py
+++ prac_03/string_formatting.py
@@ -2,3 +2,8 @@
 name = "Gibson L-5 CES"
 year = 1922
 cost = 16036
+print(f"{year} {name} for about ${cost:,}!")
+
+# Using a for loop with f-string formatting to generate the required output
+for i in range(11):
+    print(f"2 ^ {i:2} is {2 ** i:4}")
