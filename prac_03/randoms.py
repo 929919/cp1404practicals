d558a7548fb42732fc1534f82341c137063214a9
João Gabriel R Costa <jgcosta.estudante@gmail.com>
2025-02-16 14:06:46 +0800

    "The smallest number could have been 5, and the largest could have been 19"

---
+++ prac_03/randoms.py
@@ -0,0 +1,9 @@
+import random
+
+print(random.randint(5, 20))  # line 1
+
+# What did you see on line 1?
+# It will print a random integer between 5 and 20 inclusive.
+
+# What was the smallest number you could have seen, what was the largest?
+# The smallest number could have been 5, and the largest could have been 19.
