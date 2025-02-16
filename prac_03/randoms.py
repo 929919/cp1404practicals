afd8b7feaf87719b681b43c3a9ce7b8bb55d3403
João Gabriel R Costa <jgcosta.estudante@gmail.com>
2025-02-16 14:10:53 +0800

    "The smallest number could have been 2.5, and the largest could have been 5.5"

--- prac_03/randoms.py
+++ prac_03/randoms.py
@@ -14,3 +14,12 @@ print(random.randrange(3, 10, 2))  # line 2
 
 # What was the smallest number you could have seen, what was the largest?
 # The smallest number could have been 3, and the largest could have been 9.
+# Could line 2 have produced a 4?
+# No, because the step value is 2, so only odd numbers (3, 5, 7, 9) are possible.
+
+print(random.uniform(2.5, 5.5))  # line 3
+# What did you see on line 3?
+# It will print a random floating-point number between 2.5 and 5.5.
+
+# What was the smallest number you could have seen, what was the largest?
+# The smallest number could have been 2.5, and the largest could have been 5.5.
