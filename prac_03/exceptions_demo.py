f4f346d9b56ccbdd4303f345ed000d1b2e5d9016
João Gabriel R Costa <jgcosta.estudante@gmail.com>
2025-02-16 16:12:01 +0800

    "Add answer with comments"

--- prac_03/exceptions_demo.py
+++ prac_03/exceptions_demo.py
@@ -5,7 +5,6 @@ Answer the following questions:
 2. When will a ZeroDivisionError occur?
 3. Could you change the code to avoid the possibility of a ZeroDivisionError?
 """
-
 try:
     numerator = int(input("Enter the numerator: "))
     denominator = int(input("Enter the denominator: "))
@@ -16,3 +15,15 @@ except ValueError:
 except ZeroDivisionError:
     print("Cannot divide by zero!")
 print("Finished.")
+
+# Answer to question 1:
+# A ValueError will occur if the user enters something that cannot be converted to an integer,
+# for example, a string like "abc" instead of a valid number.
+
+# Answer to question 2:
+# A ZeroDivisionError will occur if the user enters 0 as the denominator,
+# because dividing by zero is undefined in mathematics and results in an error.
+
+# Answer to question 3:
+# To avoid the possibility of a ZeroDivisionError, we can check if the denominator is zero before performing the division.
+# Here's an updated version of the code to handle that:
