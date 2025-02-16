1badd3e5150c67726a53cc05be87e0ed9eceb37e
João Gabriel R Costa <jgcosta.estudante@gmail.com>
2025-02-16 14:08:32 +0800

    "The smallest number could have been 3, and the largest could have been 9"

--- prac_03/randoms.py
+++ prac_03/randoms.py
@@ -7,3 +7,10 @@ print(random.randint(5, 20))  # line 1
 
 # What was the smallest number you could have seen, what was the largest?
 # The smallest number could have been 5, and the largest could have been 19.
+print(random.randrange(3, 10, 2))  # line 2
+
+# What did you see on line 2?
+# It will print a random number in the range from 3 to 10 with a step of 2 (possible numbers: 3, 5, 7, 9).
+
+# What was the smallest number you could have seen, what was the largest?
+# The smallest number could have been 3, and the largest could have been 9.
