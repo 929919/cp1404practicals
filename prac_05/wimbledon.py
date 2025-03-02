75cab76911b0419afff2ec20a825270b49e3b4ba
João Gabriel R Costa <jgcosta.estudante@gmail.com>
2025-03-02 18:35:33 +0800

    "Add second function code"

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -7,3 +7,15 @@ def read_wimbledon_data(filename):
     """Read the Wimbledon CSV file and return data as a list of lists."""
     with open(filename, "r", encoding="utf-8-sig") as file:
         return [line.strip().split(',') for line in file.readlines()[1:]]
+
+def process_wimbledon_data(data):
+    """Process Wimbledon data to count wins per champion and track winning countries."""
+    champions = {}
+    countries = set()
+
+    for entry in data:
+        champion, country = entry[2], entry[1]
+        champions[champion] = champions.get(champion, 0) + 1
+        countries.add(country)
+
+    return champions, sorted(countries)
