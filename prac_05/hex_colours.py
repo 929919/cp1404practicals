acd4c76e104b3b590446017e4319acf5adfdf871
João Gabriel R Costa <jgcosta.estudante@gmail.com>
2025-03-02 17:20:30 +0800

    "Add hex colours"

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -3,3 +3,15 @@ Hex Colour Lookup
 Estimate: 15 minutes
 Actual:   __ minutes
 """
+HEX_COLOURS = {
+    "AliceBlue": "#F0F8FF",
+    "AntiqueWhite": "#FAEBD7",
+    "Aqua": "#00FFFF",
+    "Aquamarine": "#7FFFD4",
+    "Azure": "#F0FFFF",
+    "Beige": "#F5F5DC",
+    "Bisque": "#FFE4C4",
+    "Black": "#000000",
+    "BlanchedAlmond": "#FFEBCD",
+    "Blue": "#0000FF"
+}
