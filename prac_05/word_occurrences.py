0721290990d74aa5f774fbd7b86f605e1d43d0a4
João Gabriel R Costa <jgcosta.estudante@gmail.com>
2025-03-02 17:45:02 +0800

    "Complete word occurrences"

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -1,17 +1,20 @@
 """
 Word Occurrences
 Estimate: 15 minutes
-Actual:
+Actual: 9 minutes
 """
-def main():
-    """Count occurrences of words in a string and display results in a sorted manner."""
-    text = input("Text: ")
-    words = text.split()
-    word_counts = {}
+"""Count occurrences of words in a string and display results in a sorted manner."""
+text = input("Text: ")
+words = text.split()
+word_counts = {}
 
-    for word in words:
-        word_counts[word] = word_counts.get(word, 0) + 1
+for word in words:
+    word_counts[word] = word_counts.get(word, 0) + 1
 
-    max_length = max(len(word) for word in word_counts)
+max_length = max(len(word) for word in word_counts)
 
+for word in sorted(word_counts):
+    print(f"{word:{max_length}} : {word_counts[word]}")
+
+if __name__ == "__main__":
     main()
