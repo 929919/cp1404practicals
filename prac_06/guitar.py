7dea137514397198e00b78216f9f000be53bd62d
João Gabriel R Costa <jgcosta.estudante@gmail.com>
2025-03-12 14:25:32 +0800

    "Add get_age and is_vintage methods to Guitar class"

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -21,3 +21,11 @@ class Guitar:
     def __str__(self):
         """Return a string representation of the Guitar."""
         return f"{self.name} ({self.year}) : ${self.cost:,.2f}"
+
+    def get_age(self):
+        """Return the age of the guitar in years (as of 2025)."""
+        return 2025 - self.year
+
+    def is_vintage(self):
+        """Return True if the guitar is 50 or more years old, False otherwise."""
+        return self.get_age() >= 50
