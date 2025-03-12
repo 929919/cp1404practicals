412e4d90e8c7541317d086d7d528c5e93ce1b0b8
João Gabriel R Costa <jgcosta.estudante@gmail.com>
2025-03-12 14:00:00 +0800

    "Add __str__ method to ProgrammingLanguage class"

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -24,3 +24,7 @@ class ProgrammingLanguage:
     def is_dynamic(self):
         """Return True if the language is dynamically typed, False otherwise."""
         return self.typing == "Dynamic"
+
+    def __str__(self):
+        """Return a string representation of the ProgrammingLanguage."""
+        return f"{self.name}, {self.typing} Typing, Reflection={self.reflection}, First appeared in {self.year}"
