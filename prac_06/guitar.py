ba63d3053ea9ca73b20ea1bc2958c1fcc5c2eb78
João Gabriel R Costa <jgcosta.estudante@gmail.com>
2025-03-12 14:20:55 +0800

    "Add initial Guitar class with __init__ method"

---
+++ prac_06/guitar.py
@@ -0,0 +1,19 @@
+"""CP1404 Practical - Guitar class.
+Estimated time: 15 minutes
+Actual time: [To be recorded]
+"""
+
+
+class Guitar:
+    """Represent a guitar with its properties."""
+
+    def __init__(self, name="", year=0, cost=0):
+        """Initialize a Guitar instance.
+
+        name: str, name of the guitar
+        year: int, year the guitar was made
+        cost: float, cost of the guitar
+        """
+        self.name = name
+        self.year = year
+        self.cost = cost
