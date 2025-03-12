6c4bc3f4fe6e72ab1be50a89be78648f33f5eeae
João Gabriel R Costa <jgcosta.estudante@gmail.com>
2025-03-12 14:42:35 +0800

    "Add test data to guitars.py for efficiency"

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -1,6 +1,6 @@
 """CP1404 Practical - Guitars program.
 Estimated time: 20 minutes
-Actual time:
+Actual time: 20 minutes
 """
 from guitar import Guitar
 
@@ -8,16 +8,23 @@ def main():
     print("My guitars!")
     guitars = []
 
-    while True:
-        name = input("Name: ")
-        if name == "":
-            break
-        year = int(input("Year: "))
-        cost = float(input("Cost: $"))
-        guitar = Guitar(name, year, cost)
-        guitars.append(guitar)
-        print(f"{guitar} added.\n")
+    # Entrada do usuário (comentada para teste)
+    # while True:
+    #     name = input("Name: ")
+    #     if name == "":
+    #         break
+    #     year = int(input("Year: "))
+    #     cost = float(input("Cost: $"))
+    #     guitar = Guitar(name, year, cost)
+    #     guitars.append(guitar)
+    #     print(f"{guitar} added.\n")
 
+    # Dados de teste
+    guitars.append(Guitar("Fender Stratocaster", 2014, 765.40))
+    guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
+    guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.90))
+
+    # Exibir guitarras
     print("\nThese are my guitars:")
     for i, guitar in enumerate(guitars, 1):
         vintage_string = " (vintage)" if guitar.is_vintage() else ""
