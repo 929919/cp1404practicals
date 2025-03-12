af0fd32400a969a7d316d2bf9c0f00fdce7e7e33
João Gabriel R Costa <jgcosta.estudante@gmail.com>
2025-03-12 13:46:01 +0800

    "Add initial ProgrammingLanguage class with __init__ method"

---
+++ prac_06/programming_language.py
@@ -0,0 +1,22 @@
+"""CP1404 Intermediate Exercises - Programming Language class.
+Estimated time: 30 minutes
+Actual time: [To be recorded]
+Start time: 10:00 AM, March 11, 2025
+"""
+
+
+class ProgrammingLanguage:
+    """Represent a programming language with its properties."""
+
+    def __init__(self, name, typing, reflection, year):
+        """Initialize a ProgrammingLanguage instance.
+
+        name: str, name of the programming language
+        typing: str, 'Static' or 'Dynamic'
+        reflection: bool, whether the language supports reflection
+        year: int, year the language first appeared
+        """
+        self.name = name
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
