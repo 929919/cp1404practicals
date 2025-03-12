75cc5aaa28e2521dc7abc35bdf1ddae5963aa71c
João Gabriel R Costa <jgcosta.estudante@gmail.com>
2025-03-12 14:08:52 +0800

    "Add list of languages and loop to print dynamically typed languages"

--- prac_06/languages.py
+++ prac_06/languages.py
@@ -11,5 +11,12 @@ def main():
     visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
     print(python)
 
+    languages = [python, ruby, visual_basic]
+    print("The dynamically typed languages are:")
+    for language in languages:
+        if language.is_dynamic():
+            print(language.name)
+
+
 if __name__ == "__main__":
     main()
