d99b6fffdf567396813deb9d6d8e5a1a852b6778
João Gabriel R Costa <jgcosta.estudante@gmail.com>
2025-03-12 18:50:15 +0800

    "Some motifications"

--- prac_06/guitar_test.py
+++ prac_06/guitar_test.py
@@ -6,13 +6,11 @@ from guitar import Guitar
 
 def main():
     guitar1 = Guitar("Gibson L-5 CES", 1922, 16035.40)
-    guitar2 = Guitar("Another Guitar", 2013, 0)  # Cost não é usado nos testes
+    guitar2 = Guitar("Another Guitar", 2013, 0)
 
-    # Teste get_age()
     print(f"Gibson L-5 CES get_age() - Expected 103. Got {guitar1.get_age()}")
     print(f"Another Guitar get_age() - Expected 12. Got {guitar2.get_age()}")
 
-    # Teste is_vintage()
     print(f"Gibson L-5 CES is_vintage() - Expected True. Got {guitar1.is_vintage()}")
     print(f"Another Guitar is_vintage() - Expected False. Got {guitar2.is_vintage()}")
 
--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -8,23 +8,30 @@ def main():
     print("My guitars!")
     guitars = []
 
-    # Entrada do usuário (comentada para teste)
-    # while True:
-    #     name = input("Name: ")
-    #     if name == "":
-    #         break
-    #     year = int(input("Year: "))
-    #     cost = float(input("Cost: $"))
-    #     guitar = Guitar(name, year, cost)
-    #     guitars.append(guitar)
-    #     print(f"{guitar} added.\n")
+    # print("My guitars!")
+    # guitars = []
+    #
+    # # Simulação de entrada
+    # name = "Fender Stratocaster"
+    # year = 2014
+    # cost = 765.4
+    # guitar = Guitar(name, year, cost)
+    # guitars.append(guitar)
+    # print(f"{guitar} added.\n")
+    #
+    # name = ""  # Encerra o loop
+    #
+    # print("\nThese are my guitars:")
+    # for i, guitar in enumerate(guitars, 1):
+    #     vintage_string = " (vintage)" if guitar.is_vintage() else ""
+    #     print(f"Guitar {i}: {guitar.name:>20} ({guitar.year}), worth ${guitar.cost:10,.2f}{vintage_string}")
 
-    # Dados de teste
+
+
     guitars.append(Guitar("Fender Stratocaster", 2014, 765.40))
     guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
     guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.90))
 
-    # Exibir guitarras
     print("\nThese are my guitars:")
     for i, guitar in enumerate(guitars, 1):
         vintage_string = " (vintage)" if guitar.is_vintage() else ""
